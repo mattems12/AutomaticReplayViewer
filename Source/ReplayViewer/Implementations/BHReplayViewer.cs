dcb6ed570983d9fef79b33742283b7e9be6301cd
taylorsussex <taylor.sussex1@gmail.com>
2018-03-05 16:43:31 +1300

    Working on supporting more resolutions
    Now the images used for comparing will scale with the in game resolution
    
    Addtionally. there's a first attempt at making the mouse position work for multiple resolutions
    
    The images are scaled incorrectly for some resolutions
    
    I suspect that this is for 4:3 resolutions or close enough ones as for these ones the game places black bars above and below the screen

--- Source/ReplayViewer/Implementations/BHReplayViewer.cs
+++ Source/ReplayViewer/Implementations/BHReplayViewer.cs
@@ -19,6 +19,13 @@ class BHReplayViewer : ReplayViewer
 
         if (NoErrors)
         {
+            previousscreen = ScreenGrabber.PrintWindow(hWnd);
+            ResX = previousscreen.Width;
+            ResY = previousscreen.Height;
+
+            ResizeBicubic Resize = new ResizeBicubic(220*ResY/1080, 146*ResY/1080);
+            Title = Resize.Apply(Title);
+
             LoopThread = new Thread(() => PlaybackLoop(ReplaysToPlay, RecordStart, RecordStop));
             LoopThread.Start();
         }
@@ -34,6 +41,21 @@ class BHReplayViewer : ReplayViewer
 
         menu = ScreenGrabber.Contains(screen, Title);
 
+        /*
+         List of resolutions on my machine that this wont work correctly for
+         1280x1024
+         1280x960
+         1152x864
+         1024x768
+         800x600
+         None of these are 16:9
+         I think this is because the game decides to leave the 'bars' to fill out the vertical axis rather than the horizontal axis
+         Therefore, the title image that the game screen is compared to is incorrectly scaled and the condition is never met
+         TODO - figure out how the game decides whether to fill out the vertical or horizontal axis
+                 - maybe it will decide to fill out the vertical if the screen is small enough?
+                 - in the cases where it doesnt work the screen is filled out w black bars
+                 - most of these are 4:3 hmmm
+        */
         if (menu)
             return;
 
@@ -65,7 +87,7 @@ class BHReplayViewer : ReplayViewer
 
     protected override void NavigateDefault()
     {
-        Mouse.Move(1320, 43);    // TODO - account for possible different resolutions
+        Mouse.Move(ResX - 65*ResY/1080, 61*ResY/1080);    // TODO - account for possible different resolutions - better but still incomplete
         Thread.Sleep(20);
         Mouse.PressButton(Mouse.MouseKeys.Left);
 
@@ -91,6 +113,8 @@ class BHReplayViewer : ReplayViewer
 
     }
 
+    private int ResX;
+    private int ResY;
     private bool NoErrors;
     private Bitmap screen;
     private Bitmap previousscreen;
